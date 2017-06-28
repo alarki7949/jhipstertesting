04b392c8eb1fd8563c4b03e2aa447fae79bccf58
Annie Larkins <annie.larkins@jhu.apl.edu>
2017-06-28 13:11:48 -0400

    Added file rewrite for README

--- example/src/main/java/mustachejava/ReadMeReWriter.java
+++ example/src/main/java/mustachejava/ReadMeReWriter.java
@@ -5,32 +5,26 @@ import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 
-public class ReadMeEditor {
 
-    String projectName = "MyProject";
+public class ReadMeReWriter {
+
+    String projectName = "AnnieProject";
 
     public static void main(String[] args) throws IOException {
         MustacheFactory mf = new DefaultMustacheFactory();
         Mustache mustache = mf.compile("README.md.mustache");
-        mustache.execute(new PrintWriter(System.out), new ReadMeEditor()).flush();
-
         BufferedWriter bw = null;
 
         try{
-            String mycontent = "This is a test and" +
-                    " I hope it works!";
-
             //Create file and specify path
             File readme = new File("README.md");
 
-	        //Create file if it doesn't exist
+            //Create file if it doesn't exist
             if (!readme.exists()) {
                 readme.createNewFile();
             }
@@ -38,7 +32,9 @@ public class ReadMeEditor {
             //Initialize filewriter
             FileWriter fw = new FileWriter(readme);
             bw = new BufferedWriter(fw);
-            bw.write(mycontent);
+
+            //Print filled in mustache template
+            mustache.execute(bw, new ReadMeReWriter()).flush();
             System.out.println("File written Successfully");
 
         } catch(IOException ioe) {
