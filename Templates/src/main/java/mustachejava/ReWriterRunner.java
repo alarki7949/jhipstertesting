3cb27cadbd71a5a5c2e7de7635031b741472858d
Annie Larkins <annie.larkins@jhu.apl.edu>
2017-06-30 09:17:07 -0400

    Added a properties file and a method to read in properties from file

--- Templates/src/main/java/mustachejava/ReWriterRunner.java
+++ Templates/src/main/java/mustachejava/ReWriterRunner.java
@@ -3,18 +3,20 @@ package mustachejava;
 import java.io.IOException;
 import java.util.Scanner;
 
+import java.io.*;
+
 /**
  * Created by larkiav1 on 6/29/17.
  */
 public class ReWriterRunner {
-    String projectName1 = "My Project";
+    static String projectName;
 
     public String getProjectName1() {
-        return projectName1;
+        return projectName;
     }
 
     public void setProjectName1(String projectName1) {
-        this.projectName1 = projectName1;
+        this.projectName = projectName1;
     }
 
     public static String getPath(){
@@ -27,7 +29,29 @@ public class ReWriterRunner {
         return s;
     }
 
-    public static void main(String[] args){
+    public static String getProperty(String propertyName) throws Exception{
+        String thisLine = null;
+        String toReturn = " ";
+
+        try {
+            // open input stream test.txt for reading purpose.
+            FileReader properties = new FileReader("properties.txt");
+            BufferedReader br = new BufferedReader(properties);
+
+            while ((thisLine = br.readLine()) != null) {
+                if(thisLine.substring(0,propertyName.length()).equals(propertyName)){
+                    toReturn = thisLine;
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        toReturn = toReturn.substring(toReturn.indexOf('[')+1,toReturn.indexOf(']'));
+        return toReturn;
+
+    }
+
+    public static void main(String[] args) throws Exception{
         AngularCliReWriter run1 = new AngularCliReWriter();
         PackageReWriter run2 = new PackageReWriter();
         ReadMeReWriter run3 = new ReadMeReWriter();
@@ -35,6 +59,7 @@ public class ReWriterRunner {
         PomReWriter run5 = new PomReWriter();
 
         try{
+            projectName = getProperty("projectName");
             run1.main(null);
             run2.main(null);
             run3.main(null);
